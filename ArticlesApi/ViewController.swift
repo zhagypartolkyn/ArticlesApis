2f60d1813e85dcc338796335660b886203c26b44
Tolkyn Zhagypar <tolkyn.zhagipar@gmail.com>
2023-05-01 16:38:56 +0500

    Initial Commit

---
+++ ArticlesApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArticlesApi
+//
+//  Created by Толкын Жагыпар on 01.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
